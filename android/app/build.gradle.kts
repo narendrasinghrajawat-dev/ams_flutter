89923655a3328f78d28043b4da5dc67fac3a0b2f
narendrasinghrajawat-dev <narendrasinghrajawat.dev@gmail.com>
2025-12-04 16:29:43 +0530

    some ui changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.attedance_management_system"
+    namespace = "com.example.ams"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.attedance_management_system"
+        applicationId = "com.example.ams"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/ams/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ams/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.attedance_management_system
+package com.example.ams
 
 import io.flutter.embedding.android.FlutterActivity
 
