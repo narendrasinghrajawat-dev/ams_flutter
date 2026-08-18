11f683c4ebf767bf2adbb5a635b235dddc1d346d
narendrasinghrajawat-dev <narendrasinghrajawat.dev@gmail.com>
2026-08-18 07:28:25 +0000

    Deploying to gh-pages from @ narendrasinghrajawat-dev/ams_flutter@9be37cd53465ebfb1598858b98f1de5c56cfb97c 🚀

--- test/flutter_bootstrap.js
+++ test/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3164160345" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1712268247" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
